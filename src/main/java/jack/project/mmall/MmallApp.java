c5685572e278b891bd06cd579663a28018320e22
zzdzhou <zzd16000@163.com>
2018-11-24 08:57:14 +0800

    20181124

---
+++ src/main/java/jack/project/mmall/MmallApp.java
@@ -0,0 +1,21 @@
+package jack.project.mmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Theme:
+ * <p>
+ * Description:
+ *
+ * @author Zhengde ZHOU
+ * Created on 2018-11-21
+ */
+@SpringBootApplication
+public class MmallApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MmallApp.class);
+    }
+
+}
---
+++ src/main/java/jack/project/mmall/controller/portal/UserController.java
@@ -0,0 +1,13 @@
+package jack.project.mmall.controller.portal;
+
+/**
+ * Theme:
+ * <p>
+ * Description:
+ *
+ * @author Zhengde ZHOU
+ * Created on 2018-11-23
+ */
+
+public class UserController {
+}
